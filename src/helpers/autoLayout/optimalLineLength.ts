ac5221d9030733f9d7938f1eb615498ba44e854a
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-09-20 11:51:22 +0200

    Implement wordSpacing in optimal line-length

--- src/helpers/autoLayout/optimalLineLength.ts
+++ src/helpers/autoLayout/optimalLineLength.ts
@@ -5,6 +5,7 @@ export interface customFont {
 
 export interface LineLengthTypography {
   chars: number;
+  sample?: string;
   pageGutter?: number;
   fontFace?: string | customFont;
   letterSpacing?: number;
@@ -16,28 +17,46 @@ export interface LineLengthTypography {
 
 export const getOptimalLineLength = (typo: LineLengthTypography): number => {
   const defaultFontSize = 16;
-  const letterSpacing = typo.letterSpacing || 0;
   const padding = typo.pageGutter ? typo.pageGutter * 2 : 0;
+  const letterSpacing = typo.letterSpacing || 0;
+  const wordSpacing = typo.wordSpacing || 0;
 
   // It’s impractical or impossible to get the font in canvas 
   // so we assume it’s 0.5em wide by 1em tall
   let optimalLineLength = Math.round((typo.chars * ((defaultFontSize * 0.5) + letterSpacing)) + padding) / defaultFontSize;
 
+  const approximateWordSpaces = () => {
+    let wordSpaces = 0;
+    if (typo.sample && typo.sample.length >= typo.chars) {
+      const spaceCount = typo.sample.match(/([\s]+)/gi);
+      // Average for number of chars
+      wordSpaces = (spaceCount ? spaceCount.length : 0) * (typo.chars / typo.sample.length);
+    }
+    return wordSpaces;
+  }
+
   const measureText = (canvas: HTMLCanvasElement, fontFace: string) => {
     const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
     if (ctx) {
       // ch based on 0
-      const txt = "0".repeat(typo.chars);
+      let txt = "0".repeat(typo.chars);
       ctx.font = `${defaultFontSize}px ${fontFace}`;
 
+      if (typo.sample && typo.sample.length >= typo.chars) {
+        txt = typo.sample.slice(0, typo.chars);
+      }
+
       // Not supported in Safari
-      if (Object.hasOwn(ctx, "letterSpacing")) {
+      if (Object.hasOwn(ctx, "letterSpacing") && Object.hasOwn(ctx, "wordSpacing")) {
         ctx.letterSpacing = letterSpacing.toString() + "px";
+        ctx.wordSpacing = wordSpacing.toString() + "px";
         optimalLineLength = Math.round(ctx.measureText(txt).width + padding) / defaultFontSize;
       } else {
-        // Instead of filling text with an offset for each character
-        // We simply add it to the measured width
-        optimalLineLength = Math.round(ctx.measureText(txt).width + (letterSpacing * (typo.chars - 1)) + padding) / defaultFontSize;
+        // Instead of filling text with an offset for each character and space
+        // We simply add them to the measured width
+        const letterSpace = letterSpacing * (typo.chars - 1);
+        const wordSpace = wordSpacing * approximateWordSpaces();
+        optimalLineLength = Math.round(ctx.measureText(txt).width + letterSpace + wordSpace + padding) / defaultFontSize;
       }
     }
   }
