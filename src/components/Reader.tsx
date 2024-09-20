6bf358763eb65d0723c5816ede795095ac0e5001
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-09-20 11:51:22 +0200

    Implement wordSpacing in optimal line-length

--- src/components/Reader.tsx
+++ src/components/Reader.tsx
@@ -72,7 +72,10 @@ export const Reader = ({ rawManifest, selfHref }: { rawManifest: object, selfHre
         optimalLineLength = getOptimalLineLength({
           chars: RSdefaults.lineLength,
           fontFace: fontStacks.RS__oldStyleTf,
-          pageGutter: RSdefaults.pageGutter
+          pageGutter: RSdefaults.pageGutter,
+        //  letterSpacing: 2,
+        //  wordSpacing: 2,
+        //  sample: "It will be seen that this mere painstaking burrower and grub-worm of a poor devil of a Sub-Sub appears to have gone through the long Vaticans and street-stalls of the earth, picking up whatever random allusions to whales he could anyways find in any book whatsoever, sacred or profane. Therefore you must not, in every case at least, take the higgledy-piggledy whale statements, however authentic, in these extracts, for veritable gospel cetology. Far from it. As touching the ancient authors generally, as well as the poets here appearing, these extracts are solely valuable or entertaining, as affording a glancing bird’s eye view of what has been promiscuously said, thought, fancied, and sung of Leviathan, by many nations and generations, including our own."
         });
         handleResize();
       }
