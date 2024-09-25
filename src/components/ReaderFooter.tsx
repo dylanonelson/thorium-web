2402631b61897e1f8d32924244466aadc8dc58f1
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-09-25 09:05:33 +0200

    Add locale for footer

--- src/components/ReaderFooter.tsx
+++ src/components/ReaderFooter.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Locale from "../resources/locales/en.json";
+
 export const ReaderFooter = ({className}: {className: string | undefined}) => {
   return (
     <>
-      <footer className={className ? className : ""} id="bottom-bar" aria-label="Bottom Bar">
+      <footer className={className ? className : ""} id="bottom-bar" aria-label={Locale.reader.app.footer}>
       </footer>
     </>);
 }
