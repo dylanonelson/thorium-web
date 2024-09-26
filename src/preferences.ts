a5f725bf710bb101f2d3a07a9456d2d049fafe16
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-09-26 11:50:17 +0200

    Add comment in preferences

--- src/preferences.ts
+++ src/preferences.ts
@@ -2,5 +2,5 @@ export const RSPrefs = {
   breakpoint: 1024, // width in pixels
   lineLength: 60, // number of characters
   pageGutter: 20, // body padding in px
-  arrowSize: 40
+  arrowSize: 40 // Size of the left and right arrows in px
 }
