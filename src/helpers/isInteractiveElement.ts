cdbb87cc30656e074820f9a3b6e966867e43d51e
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-09-27 17:06:02 +0200

    Improve isInteractiveElement helper

--- src/helpers/isInteractiveElement.ts
+++ src/helpers/isInteractiveElement.ts
@@ -1,13 +1,14 @@
 export const isInteractiveElement = (element: Element | null) => {
   let interactive = false;
 
-  const iElements = ["A", "AREA", "BUTTON", "INPUT", "SELECT", "TEXTAREA"];
+  const iElements = ["A", "AREA", "BUTTON", "DETAILS", "INPUT", "SELECT", "TEXTAREA"];
 
   if (element) {
     if (iElements.includes(element.tagName)) interactive = true;
     if (element.hasAttribute("disabled")) interactive = false;
-    if (element.getAttribute("tabindex") === "-1") interactive = false;
-    if (element.getAttribute("tabindex") === "0") interactive = true;
+    if (element.hasAttribute("tabindex")) {
+      interactive = Number(element.getAttribute("tabindex")) >= 0; 
+    };
   }
 
   return interactive;
