2735e61ed1d55401579c48d2288be587542b2817
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-10-09 15:26:35 +0200

    Move settings style to module

--- src/components/ReaderHeader.tsx
+++ src/components/ReaderHeader.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Locale from "../resources/locales/en.json";
+import settingsStyles from "./assets/styles/readerSettings.module.css";
+
+import classNames from "classnames";
 
 import { ReaderSettings } from "./ReaderSettings";
 
 export const ReaderHeader = ({ className, title, isFXL }: { className: string, title: string | undefined, isFXL: boolean }) => {
   return (
     <>
-      <header className={className ? className : ""} id="top-bar" aria-label="Top Bar">
+      <header className={classNames(settingsStyles.header, className ? className : "")} id="top-bar" aria-label="Top Bar">
         <h1 aria-label={Locale.reader.app.header.title}>
           {title
             ? title
