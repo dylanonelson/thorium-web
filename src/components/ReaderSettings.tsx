a45a943b1fd69a77db333a0d2bf6cac7efe12e6e
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-10-10 18:08:51 +0200

    handle dialog onOpenChange
    [WIP] Global Reader State

--- src/components/ReaderSettings.tsx
+++ src/components/ReaderSettings.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import GearIcon from "./assets/icons/gear-icon.svg";
 import settingsStyles from "./assets/styles/readerSettings.module.css";
 
 import { ReadingDisplayLayout } from "./ReadingDisplayLayout";
 import { Button, Dialog, DialogTrigger, Popover } from "react-aria-components"; 
+import { ReaderState } from "@/app-context/readerState";
 
 export const ReaderSettings = ({ isFXL }: { isFXL: boolean }) => {
+  const { updateState } = useContext(ReaderState);
+
+  const toggleSettingsState = (value: boolean) => {
+    updateState({ settingsOpen: value });
+  }
 
   return(
     <>
-    <DialogTrigger>
+    <DialogTrigger onOpenChange={(val) => toggleSettingsState(val)}>
       <Button className={settingsStyles.gearButton}>
         <GearIcon aria-hidden="true" focusable="false" />
       </Button>
