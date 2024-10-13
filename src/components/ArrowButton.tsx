be502cc0f867f031044d669f9630aa2a4e68e7a9
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-10-13 11:23:35 +0200

    Move Reader states

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 
 import Locale from "../resources/locales/en.json";
 import arrowStyles from "./assets/styles/arrowButton.module.css";
+import readerStateStyles from "./assets/styles/readerStates.module.css";
 
 import LeftArrow from "./assets/icons/baseline-arrow_left_ios-24px.svg";
 import RightArrow from "./assets/icons/baseline-arrow_forward_ios-24px.svg";
@@ -9,17 +10,35 @@ import RightArrow from "./assets/icons/baseline-arrow_forward_ios-24px.svg";
 import { control } from "../helpers/control";
 import { Button, Tooltip, TooltipTrigger } from "react-aria-components";
 
+import { useAppSelector } from "@/lib/hooks";
+
+import classNames from "classnames";
+
 export interface ReaderArrowProps {
   direction: "left" | "right";
-  isRTL: boolean;
-  className: string;
+  className?: string;
   disabled: boolean;
 }
 
 export const ArrowButton = (props: ReaderArrowProps) => {
   const button = useRef<HTMLButtonElement>(null);
+  const isImmersive = useAppSelector(state => state.reader.isImmersive);
+  const isFullscreen = useAppSelector(state => state.reader.isFullscreen);
+  const hasReachedBreakpoint = useAppSelector(state => state.reader.hasReachedBreakpoint);
+  const isRTL = useAppSelector(state => state.reader.isRTL) || false;
 
-  const label = (props.direction === "right" || props.isRTL) ? Locale.reader.navigation.goForward : Locale.reader.navigation.goBackward;
+  const label = (props.direction === "right" && !isRTL || props.direction === "left" && isRTL) ? Locale.reader.navigation.goForward : Locale.reader.navigation.goBackward;
+
+  const handleClassNameFromState = () => {
+    console.log(isImmersive, hasReachedBreakpoint)
+    let className = "";
+    if (isImmersive && !hasReachedBreakpoint || isFullscreen) {
+      className = readerStateStyles.immersiveHidden;
+    } else if (isImmersive) {
+      className = readerStateStyles.immersive;
+    }
+    return className
+  }
 
   useEffect(() => {
     if (props.disabled && document.activeElement === button.current) {
@@ -34,7 +53,7 @@ export const ArrowButton = (props: ReaderArrowProps) => {
           ref={button}
           aria-label={label}
           onPress={() => { props.direction === "left" ? control("goLeft") : control("goRight") }}
-          className={props.className}
+          className={classNames(props.className, handleClassNameFromState())}
           isDisabled={props.disabled}>
           {props.direction === "left" ? <LeftArrow aria-hidden="true" focusable="false" /> : <RightArrow aria-hidden="true" focusable="false" />}
         </Button>
--- src/components/ReaderHeader.tsx
+++ src/components/ReaderHeader.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 import Locale from "../resources/locales/en.json";
 import settingsStyles from "./assets/styles/readerSettings.module.css";
+import readerStateStyles from "./assets/styles/readerStates.module.css";
+import { useAppSelector } from "@/lib/hooks";
 
 import classNames from "classnames";
 
 import { ReaderSettings } from "./ReaderSettings";
 
-export const ReaderHeader = ({ className, title, isFXL }: { className: string, title: string | undefined, isFXL: boolean }) => {
+export const ReaderHeader = ({ title }: { title: string | undefined }) => {
+  const isImmersive = useAppSelector(state => state.reader.isImmersive);
+  const isFXL = useAppSelector(state => state.reader.isFXL) || false;
+
   return (
     <>
-      <header className={classNames(settingsStyles.header, className ? className : "")} id="top-bar" aria-label="Top Bar">
+      <header className={classNames(settingsStyles.header, isImmersive ? readerStateStyles.immersive : "")} id="top-bar" aria-label="Top Bar">
         <h1 aria-label={Locale.reader.app.header.title}>
           {title
             ? title
