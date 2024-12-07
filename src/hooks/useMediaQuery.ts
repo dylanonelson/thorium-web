341def3abb80009a4faf79baddf3c386c2d9e5a8
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-12-07 10:49:23 +0100

    Return early if query is null in useMediaQuery

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 
-export const useMediaQuery = (query: string) => {
+export const useMediaQuery = (query: string | null) => {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
+    if (!query) return; 
+
     const media = window.matchMedia(query);
 
     if (media.matches !== matches) {
