ae032434f7edee075c15e107acea909d095056ee
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-12-09 15:48:09 +0100

    Add sample books

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,12 @@ export default function Home() {
           <Link href="/read?book=https%3A%2F%2Fpublication-server.readium.org%2FbW9ieS1kaWNrLmVwdWI">Moby Dick (reflow)</Link>
         </li>
         <li>
+          <Link href="/read?book=https%3A%2F%2Fpublication-server.readium.org%2FbmF0aGFuaWVsLWhhd3Rob3JuZV90aGUtaG91c2Utb2YtdGhlLXNldmVuLWdhYmxlc19hZHZhbmNlZC5lcHVi">The House of the Seven Gables (reflow advanced)</Link>
+        </li>
+        <li>
+          <Link href="/read?book=https%3A%2F%2Fpublication-server.readium.org%2FbGVzX2RpYWJvbGlxdWVzLmVwdWI">Les Diaboliques (reflow french)</Link>
+        </li>
+        <li>
           <Link href="/read?book=https%3A%2F%2Fpublication-server.readium.org%2FQmVsbGFPcmlnaW5hbDMuZXB1Yg">Bella the Dragon (FXL)</Link>
         </li>
       </ul>
