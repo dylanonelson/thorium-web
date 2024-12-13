d441574da8a31a5f3c6fcd3032a4cfb4c5a2def5
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-12-13 09:56:16 +0100

    Slightly improving useMediaQuery hook

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -6,16 +6,25 @@ export const useMediaQuery = (query: string | null) => {
   useEffect(() => {
     if (!query) return; 
 
-    const media = window.matchMedia(query);
+    const mq = window.matchMedia(query);
 
-    if (media.matches !== matches) {
-      setMatches(media.matches);
+    // Checking if media query is supported or well-formed
+    // The media property is the normalized and resolved string representation of the query. 
+    // If matchMedia encounters something it doesn’t understand, that changes to "not all"
+    const resolvedMediaQuery = mq.media;
+    if (query !== resolvedMediaQuery) {
+      console.error("Either this query is not supported or not well formed. Please double-check.");
+      return;
+    };
+
+    if (mq.matches !== matches) {
+      setMatches(mq.matches);
     }
 
-    const handleMatch = () => setMatches(media.matches);
-    media.addEventListener("change", handleMatch);
+    const handleMatch = () => setMatches(mq.matches);
+    mq.addEventListener("change", handleMatch);
 
-    return () => media.removeEventListener("change", handleMatch);
+    return () => mq.removeEventListener("change", handleMatch);
   }, [matches, query]);
 
   return matches;
