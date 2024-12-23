139927d34b5921734cb62e9f5a4a5741f268bdd0
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-12-23 09:25:59 +0100

    Enforce max-height for popover sheet

--- src/components/Sheets/PopoverSheet.tsx
+++ src/components/Sheets/PopoverSheet.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 
 import readerSharedUI from "../assets/styles/readerSharedUI.module.css";
 
 import CloseIcon from "../assets/icons/close.svg";
 
-import { ISheet } from "./Sheet";
 import { Button, Dialog, DialogTrigger, Popover, PopoverProps } from "react-aria-components";
+
+import { ISheet } from "./Sheet";
 
 export interface IPopoverSheet extends ISheet {
   placement?: PopoverProps["placement"];
@@ -21,6 +22,13 @@ export const PopoverSheet: React.FC<IPopoverSheet> = ({
     placement, 
     children 
   }) => {
+    const popoverRef = useRef<HTMLDivElement | null>(null);
+
+    const computeMaxHeight = useCallback(() => {
+      if (!popoverRef.current) return;
+
+      return window.innerHeight - popoverRef.current.offsetTop;
+    }, [])
 
   return (
   <>
@@ -28,10 +36,12 @@ export const PopoverSheet: React.FC<IPopoverSheet> = ({
     ? <DialogTrigger>
         { renderActionIcon() }
         <Popover 
+          ref={ popoverRef }
           placement={ placement || "bottom" }
           className={ className }
           isOpen={ isOpen }
           onOpenChange={ onOpenChangeCallback } 
+          maxHeight={ computeMaxHeight() }
         >
           <Dialog>
             <Button 
