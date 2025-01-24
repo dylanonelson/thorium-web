5e777002b4c9ab97cac5801a732a69b1753ed0f8
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-01-24 12:45:32 +0100

    Fix width errors for dock panels

--- src/hooks/useRezisablePanel.ts
+++ src/hooks/useRezisablePanel.ts
@@ -7,13 +7,23 @@ import { Docked, IDockedPref } from "../models/docking";
 import { useActions } from "./useActions";
 
 export const useRezisablePanel = (panel: Docked) => {
+  const defaultWidth = RSPrefs.docking.defaultWidth;
+  const [pref, setPref] = useState<IDockedPref | null>(null);
+
   const actions = useActions();
 
-  const [pref, setPref] = useState<IDockedPref | null>(null);
-    
-  const width = panel.width ? ((panel.width * window.innerWidth) / 100) : pref?.width || RSPrefs.docking.defaultWidth;
-  const minWidth = pref?.minWidth && pref.minWidth < width ? pref.minWidth : RSPrefs.docking.defaultWidth;
-  const maxWidth = pref?.maxWidth && pref.maxWidth > width ? pref.maxWidth : RSPrefs.docking.defaultWidth;
+  const previousWidth = panel.width || null;
+  const width = pref?.width || defaultWidth;
+  const minWidth = pref?.minWidth && pref.minWidth < width 
+    ? pref.minWidth 
+    : defaultWidth < width 
+      ? defaultWidth
+      : width;
+  const maxWidth = pref?.maxWidth && pref.maxWidth > width 
+    ? pref.maxWidth 
+    : defaultWidth > width
+      ? defaultWidth
+      : width;
 
   const isPopulated = () => {
     return panel.active && actions.isOpen(panel.actionKey);
@@ -24,20 +34,38 @@ export const useRezisablePanel = (panel: Docked) => {
   }
 
   const isResizable = () => {
-    return isPopulated() ? (width > minWidth) && (width < maxWidth) : false;
+    return isPopulated() ? Math.round(width) > Math.round(minWidth) && Math.round(width) < Math.round(maxWidth) : false;
   };
 
   const getWidth = () => {
-    return isPopulated() ? (width / window.innerWidth) * 100 : 0;
+    return isPopulated() 
+      ? previousWidth 
+        ? Math.round((previousWidth / window.innerWidth) * 100) 
+        : Math.round((width / window.innerWidth) * 100) 
+      : 0;
   };
 
   const getMinWidth = () => {
-    return isPopulated() ? (minWidth / window.innerWidth) * 100 : 0;
+    return isPopulated() ? Math.round((minWidth / window.innerWidth) * 100) : 0;
   };
 
   const getMaxWidth = () => {
-    return isPopulated() ? (maxWidth / window.innerWidth) * 100 : 0;
+    return isPopulated() ? Math.round((maxWidth / window.innerWidth) * 100) : 0;
   };
+
+  const getCurrentPxWidth = (percentage: number) => {
+    let current = Math.round((percentage * window.innerWidth) / 100);
+    
+    if (current < minWidth) {
+      current = minWidth;
+    }
+    
+    if (current > maxWidth) {
+      current = maxWidth;
+    }
+    
+    return current;
+  }
 
   // When the docked action changes, we need to update its preferences 
   useEffect(() => {
@@ -50,6 +78,7 @@ export const useRezisablePanel = (panel: Docked) => {
     currentKey, 
     getWidth,
     getMinWidth,
-    getMaxWidth
+    getMaxWidth,
+    getCurrentPxWidth
   }
 }
