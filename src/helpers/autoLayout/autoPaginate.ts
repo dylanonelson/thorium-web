a23833b33d99f313736ef0a8441c57607509d3b5
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-02-27 16:18:53 +0100

    Remove auto-pagination helpers

--- src/helpers/autoLayout/autoPaginate.ts
+++
@@ -1,5 +0,0 @@
-export const autoPaginate = (width: number, lineLength: number): number => {
-  const defaultFontSize = 16;
-  width = width / defaultFontSize;
-  return (width >= lineLength) ? Math.floor(width / lineLength) : 1;
-}
