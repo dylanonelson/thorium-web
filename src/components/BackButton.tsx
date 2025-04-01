c5c6f300805b0e95756c8ed94c17805643ed5cbb
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-04-01 15:03:58 +0200

    Settings SubPanels update
    - Change backbutton to text
    - handle Escape key in subpanels

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,18 +1,10 @@
-import { RSPrefs } from "@/preferences";
-
 import Locale from "../resources/locales/en.json";
 
 import { IBackButton } from "@/models/actions";
-import { LayoutDirection } from "@/models/layout";
 
 import readerSharedUI from "./assets/styles/readerSharedUI.module.css";
 
-import ArrowLeftIcon from "./assets/icons/arrow_back.svg";
-import ArrowRightIcon from "./assets/icons/arrow_forward.svg";
-
-import { Button, Tooltip, TooltipTrigger } from "react-aria-components";
-
-import { useAppSelector } from "@/lib/hooks";
+import { Button } from "react-aria-components";
 
 import classNames from "classnames";
 
@@ -22,38 +14,17 @@ export const BackButton = ({
   label,
   onPressCallback
 }: IBackButton) => {
-  const isRTL = useAppSelector(state => state.reader.direction) === LayoutDirection.rtl;
   
   return (
     <>
-    <TooltipTrigger
-      { ...(RSPrefs.theming.icon.tooltipDelay 
-        ? { 
-            delay: RSPrefs.theming.icon.tooltipDelay,
-            closeDelay: RSPrefs.theming.icon.tooltipDelay
-          } 
-        : {}
-      )}
+    <Button 
+      ref={ ref }
+      className={ classNames(className, readerSharedUI.backButton) } 
+      aria-label={ label || Locale.reader.app.back.trigger } 
+      onPress={ onPressCallback }
     >
-      <Button 
-        ref={ ref }
-        className={ classNames(className, readerSharedUI.backButton) } 
-        aria-label={ label || Locale.reader.app.back.trigger } 
-        onPress={ onPressCallback }
-      >
-        { isRTL 
-          ? <ArrowRightIcon aria-hidden="true" focusable="false" /> 
-          : <ArrowLeftIcon aria-hidden="true" focusable="false" /> 
-        }
-      </Button>
-      <Tooltip
-        className={ readerSharedUI.tooltip }
-        placement="bottom" 
-        offset={ RSPrefs.theming.icon.tooltipOffset || 0 }
-      >
-        { Locale.reader.app.back.tooltip }
-      </Tooltip>
-    </TooltipTrigger>
+      { Locale.reader.app.back.trigger }
+    </Button>
     </>
   )
 }
