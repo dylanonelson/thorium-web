b6191ce81bd868290aa7bbbc970609c8a2131cbd
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-04-01 18:02:26 +0200

    Add arrow back in BackButton

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,10 +1,16 @@
 import Locale from "../resources/locales/en.json";
 
 import { IBackButton } from "@/models/actions";
+import { LayoutDirection } from "@/models/layout";
+
+import ArrowLeft from "./assets/icons/arrow_back.svg";
+import ArrowRight from "./assets/icons/arrow_forward.svg";
 
 import readerSharedUI from "./assets/styles/readerSharedUI.module.css";
 
 import { Button } from "react-aria-components";
+
+import { useAppSelector } from "@/lib/hooks";
 
 import classNames from "classnames";
 
@@ -14,7 +20,8 @@ export const BackButton = ({
   label,
   onPressCallback
 }: IBackButton) => {
-  
+  const isRTL = useAppSelector(state => state.reader.direction) === LayoutDirection.rtl;
+
   return (
     <>
     <Button 
@@ -23,6 +30,9 @@ export const BackButton = ({
       aria-label={ label || Locale.reader.app.back.trigger } 
       onPress={ onPressCallback }
     >
+      { isRTL 
+        ? <ArrowRight aria-hidden="true" focusable="false" /> 
+        : <ArrowLeft aria-hidden="true" focusable="false" /> }
       { Locale.reader.app.back.trigger }
     </Button>
     </>
