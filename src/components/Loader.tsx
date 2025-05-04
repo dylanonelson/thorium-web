3890d77b7d8124c4ae1642fcfa83fa9f6c773d1a
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-05-04 15:39:52 +0200

    Add ThLoader to packages

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,23 +4,18 @@ import Locale from "../resources/locales/en.json";
 
 import readerLoaderStyles from "./assets/styles/readerLoader.module.css";
 
-// Since we are removing readerLoader entirely, no need for aria-hidden={ !isLoading }
-// No need for a label either since we are using the string for the animation
+import { ThLoader } from "@/packages/Components/Reader/ThLoader";
+
 export const Loader = ({ isLoading, children }: { isLoading: boolean, children: ReactNode }) => {
   return (
     <>
-    <div 
+    <ThLoader 
+      isLoading={ isLoading } 
+      loader={ <div className={ readerLoaderStyles.readerLoader}>{ Locale.reader.app.loading }</div> } 
       className={ readerLoaderStyles.readerLoaderWrapper } 
-      aria-busy={ isLoading } 
-      aria-live="polite"
     >
-      { isLoading && 
-        <div className={ readerLoaderStyles.readerLoader}>
-          { Locale.reader.app.loading }
-        </div> 
-      }
       { children }
-    </div>
+    </ThLoader>
     </>
   )
 }
--- src/packages/Components/Reader/index.ts
+++ src/packages/Components/Reader/index.ts
@@ -1,2 +1,3 @@
 export * from "./ThFooter";
+export * from "./ThLoader";
 export * from "./ThHeader";
