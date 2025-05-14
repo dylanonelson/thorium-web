4de3524b0b9f26e9d89c987147b47e41dbe1de61
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-05-14 14:49:01 +0200

    Make PReference Provider a generic
    So that it can accept extended preferences

--- src/preferences/ThPreferencesProvider.tsx
+++ src/preferences/ThPreferencesProvider.tsx
@@ -5,15 +5,18 @@ import { defaultPreferences } from "./defaultPreferences";
 
 import { ThPreferences } from "./preferences";
 
-const PreferencesContext = createContext(defaultPreferences);
+const PreferencesContext = createContext<ThPreferences>(defaultPreferences);
 
-export default function ThPreferencesProvider({ value, children }: {
-  value?: ThPreferences,
-  children: React.ReactNode
+export default function ThPreferencesProvider<T extends ThPreferences>({
+  value,
+  children,
+}: {
+  value?: T;
+  children: React.ReactNode;
 }) {
   return (
-    <PreferencesContext.Provider value={ value || defaultPreferences }>
-      { children }
+    <PreferencesContext.Provider value={value || (defaultPreferences)}>
+      {children}
     </PreferencesContext.Provider>
   );
 };
