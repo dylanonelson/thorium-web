94f93dd43b524405e001b1684f852c8ccd86e54d
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-06-13 17:15:28 +0200

    Top bar scroll
    There are still issues though: hovering does not work that great, and sometimes isImmersive is stale on desktop until you click on contents for some reason

--- src/components/StatefulReaderHeader.tsx
+++ src/components/StatefulReaderHeader.tsx
@@ -19,6 +19,8 @@ import { usePreferences } from "@/preferences/hooks";
 import { setHovering } from "@/lib/readerReducer";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 
+import classNames from "classnames";
+
 export const StatefulReaderHeader = () => {
   const { reflowActionKeys, fxlActionKeys } = usePreferenceKeys();
   const RSPrefs = usePreferences();
