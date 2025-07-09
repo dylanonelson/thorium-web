48c5a383f782b673a84d0374dffd8991dacf2794
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-07-09 16:51:09 +0200

    Sync columnCount
    Resolves #31

--- src/components/Epub/Settings/StatefulColumns.tsx
+++ src/components/Epub/Settings/StatefulColumns.tsx
@@ -1,37 +1,57 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import Locale from "../../../resources/locales/en.json";
 
 import AutoLayoutIcon from "./assets/icons/document_scanner.svg";
 import OneColIcon from "./assets/icons/article.svg";
 import TwoColsIcon from "./assets/icons/menu_book.svg";
-
 import { StatefulRadioGroup } from "../../Settings/StatefulRadioGroup";
 
+import { useAppSelector, useAppDispatch } from "@/lib/hooks";
+import { setColumnCount } from "@/lib/settingsReducer";
 import { useEpubNavigator } from "@/core/Hooks/Epub/useEpubNavigator";
 
-import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import { setColumnCount } from "@/lib/settingsReducer";
+import debounce from "debounce";
 
 export const StatefulColumns = () => {
   const scroll = useAppSelector(state => state.settings.scroll);
   const isFXL = useAppSelector(state => state.publication.isFXL);
   const isScroll = scroll && !isFXL;
   const columnCount = useAppSelector(state => state.settings.columnCount) || "auto";
-  const dispatch = useAppDispatch();
+  const [effectiveValue, setEffectiveValue] = useState(columnCount);
 
-  const { submitPreferences } = useEpubNavigator();
+  const dispatch = useAppDispatch();
+  const { submitPreferences, getSetting } = useEpubNavigator();
+
+  const updateEffectiveValue = useCallback((preference: string, setting: number | null) => {
+    const derivedValue = preference === "auto" || setting === null ? "auto" : setting.toString();
+    setEffectiveValue(derivedValue);
+  }, []);
 
   const updatePreference = useCallback(async (value: string) => {
     const colCount = value === "auto" ? null : Number(value);
-
     await submitPreferences({ columnCount: colCount });
-    
-    // TODO: See how best to handle this for 2 -> 1 column if minimal null
+    updateEffectiveValue(value, getSetting("columnCount"));
     dispatch(setColumnCount(value));
-  }, [submitPreferences, dispatch]);
+  }, [submitPreferences, getSetting, updateEffectiveValue, dispatch]);
+
+  // Debounce the resize handler
+  const debouncedUpdate = useCallback(() => {
+    const update = () => updateEffectiveValue(columnCount, getSetting("columnCount"));
+    debounce(update, 50)();
+  }, [columnCount, getSetting, updateEffectiveValue]);
+
+  useEffect(() => {
+    debouncedUpdate();
+
+    window.addEventListener("resize", debouncedUpdate);
+
+    return () => {
+      window.removeEventListener("resize", debouncedUpdate);
+    };
+  }, [debouncedUpdate]);
 
   return (
     <>
@@ -39,7 +59,7 @@ export const StatefulColumns = () => {
       standalone={ true }
       label={ Locale.reader.settings.column.title }
       orientation="horizontal"
-      value={ columnCount }
+      value={ effectiveValue }
       onChange={ async (val: string) => await updatePreference(val) }
       isDisabled={ isScroll }
       items={[
@@ -56,7 +76,11 @@ export const StatefulColumns = () => {
         {
           icon: TwoColsIcon,
           label: Locale.reader.settings.column.two, 
-          value: "2" 
+          value: "2",
+          // This is subpar when the columnCount is 1 though because
+          // it won’t be disabled, but it’s the best we can do with
+          // the preferences API at the moment
+          isDisabled: effectiveValue === "1" && columnCount === "2"
         }
       ]}
     />
