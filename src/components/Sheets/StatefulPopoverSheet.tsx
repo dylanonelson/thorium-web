e6b21a624a01c187c5e2751053d4f4190d01098b
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-07-30 12:33:43 +0200

    Adjustments

--- src/components/Sheets/StatefulPopoverSheet.tsx
+++ src/components/Sheets/StatefulPopoverSheet.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 
 import { StatefulSheet } from "./models/sheets";
 import { ThSheetHeaderVariant } from "@/preferences/models/enums";
