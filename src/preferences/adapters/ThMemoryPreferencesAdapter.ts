bd0aa837d437feeaa18461b99d83a36873a3ed98
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-09-15 17:50:29 +0200

    [Consistency] Pref Adapters naming

--- src/preferences/adapters/ThMemoryPreferencesAdapter.ts
+++ src/preferences/adapters/ThMemoryPreferencesAdapter.ts
@@ -4,23 +4,23 @@ import { ThPreferencesAdapter } from "./ThPreferencesAdapter";
 import { mapPreferencesToState } from "@/lib/helpers/mapPreferences";
 
 export class ThMemoryPreferencesAdapter<T extends CustomizableKeys = CustomizableKeys> implements ThPreferencesAdapter<T> {
-  private preferences: ThPreferences<T>;
+  private currentPreferences: ThPreferences<T>;
   private normalizedState: PreferencesReducerState;
   private listeners: Set<(prefs: ThPreferences<T>) => void> = new Set();
 
   constructor(initialPreferences: ThPreferences<T>) {
-    this.preferences = { ...initialPreferences };
-    this.normalizedState = mapPreferencesToState(this.preferences);
+    this.currentPreferences = { ...initialPreferences };
+    this.normalizedState = mapPreferencesToState(this.currentPreferences);
   }
 
   public getPreferences(): ThPreferences<T> {
-    return { ...this.preferences };
+    return { ...this.currentPreferences };
   }
 
   public setPreferences(prefs: ThPreferences<T>): void {
-    this.preferences = { ...prefs };
-    this.normalizedState = mapPreferencesToState(this.preferences);
-    this.notifyListeners(this.preferences);
+    this.currentPreferences = { ...prefs };
+    this.normalizedState = mapPreferencesToState(this.currentPreferences);
+    this.notifyListeners(this.currentPreferences);
   }
 
   public subscribe(listener: (prefs: ThPreferences<T>) => void): void {
