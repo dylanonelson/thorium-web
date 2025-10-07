176fb9cc98c8bdd7032a7d5a34053f24846c7b9d
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-10-07 16:54:56 +0200

    Change FontWeight to Unstable RadioGroup
    Resolves #124

--- src/components/Epub/Settings/Text/StatefulFontWeight.tsx
+++ src/components/Epub/Settings/Text/StatefulFontWeight.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useCallback } from "react";
+import { useMemo, useCallback } from "react";
 
 import { StatefulSettingsItemProps } from "../../../Settings/models/settings";
-import { fontWeightRangeConfig } from "@readium/navigator";
 
-import { StatefulSlider } from "../../../Settings/StatefulSlider";
+import DefaultIcon from "./assets/icons/format_bold_wght200.svg";
+import BolderIcon from "./assets/icons/format_bold_wght500.svg";
+
+import { StatefulRadioGroup } from "@/components/Settings/StatefulRadioGroup";
 
 import { useEpubNavigator } from "@/core/Hooks/Epub/useEpubNavigator";
 import { useI18n } from "@/i18n/useI18n";
@@ -13,32 +15,57 @@ import { useI18n } from "@/i18n/useI18n";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { setFontWeight } from "@/lib/settingsReducer";
 
-export const StatefulFontWeight = ({ standalone = true }: StatefulSettingsItemProps) => {
+type FontWeight = "default" | "bolder";
+
+export const UnstableStatefulFontWeight = ({ standalone = true }: StatefulSettingsItemProps) => {
   const { t } = useI18n();
-  const fontFamily = useAppSelector(state => state.settings.fontFamily);
   const fontWeight = useAppSelector(state => state.settings.fontWeight);
+
   const dispatch = useAppDispatch();
 
   const { getSetting, submitPreferences } = useEpubNavigator();
 
-  const updatePreference = useCallback(async (value: number) => {
-    await submitPreferences({ fontWeight: value });
+  const items = [
+    {
+      id: "default",
+      icon: DefaultIcon,
+      label: t("reader.settings.fontWeight.default"), 
+      value: "default" 
+    },
+    {
+      id: "bolder",
+      icon: BolderIcon,
+      label: t("reader.settings.fontWeight.bolder"), 
+      value: "bolder" 
+    }
+  ];
+
+  const derivedValue = useMemo(() => {
+    if (fontWeight === 400) {
+      return "default";
+    } else if (fontWeight === 700) {
+      return "bolder";
+    }
+    return "default";
+  }, [fontWeight]);
+
+  const updatePreference = useCallback(async (value: FontWeight) => {
+    const fontWeightValue = value === "default" ? 400 : 700;
+    await submitPreferences({ fontWeight: fontWeightValue });
 
     dispatch(setFontWeight(getSetting("fontWeight")));
   }, [submitPreferences, getSetting, dispatch]);
 
   return(
     <>
-    <StatefulSlider
-      standalone={ standalone }
+    <StatefulRadioGroup 
+      standalone={ standalone } 
       label={ t("reader.settings.fontWeight.title") }
-      defaultValue={ 400 } 
-      value={ fontWeight } 
-      onChange={ async(value) => await updatePreference(value as number) } 
-      range={ fontWeightRangeConfig.range }
-      step={ fontWeightRangeConfig.step }
-      isDisabled={ fontFamily === "publisher" }
-    /> 
+      orientation="horizontal" 
+      value={ derivedValue } 
+      onChange={ async (val: string) => await updatePreference(val as FontWeight) }
+      items={ items }
+    />  
     </>
   )
 }
